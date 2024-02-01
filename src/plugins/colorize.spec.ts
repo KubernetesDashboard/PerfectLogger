0271468beb1bcadca712cef9c9633028e6c0e6c8
MDReal32 <veysaliyev00@gmail.com>
2024-02-01 09:50:52 +0400

    chore(task-14): fix tests
    (cherry picked from commit f470bf3b206482bdba9484bc3dc27b8964de9e77)

--- src/plugins/colorize.spec.ts
+++ src/plugins/colorize.spec.ts
@@ -1,14 +1,32 @@
+import chalk from "chalk";
+import { describe, expect, it } from "vitest";
+
 import { LogLevel } from "../types/log-level";
 import { colorize } from "./colorize";
 
 describe("colorize plugin", () => {
-  it("should return the same message for each log level", () => {
-    const input = "Hello, world!";
-    const levels = [LogLevel.DEBUG, LogLevel.INFO, LogLevel.LOG, LogLevel.WARN, LogLevel.ERROR];
+  it("should colorize the debug level", () => {
+    const result = colorize().message("Hello World!", LogLevel.DEBUG);
+    expect(result).toBe(chalk.blue("Hello World!"));
+  });
 
-    levels.forEach(level => {
-      const output = colorize().message(input, level);
-      expect(output).toEqual(input);
-    });
+  it("should colorize the info level", () => {
+    const result = colorize().message("Hello World!", LogLevel.INFO);
+    expect(result).toBe(chalk.green("Hello World!"));
+  });
+
+  it("should colorize the log level", () => {
+    const result = colorize().message("Hello World!", LogLevel.LOG);
+    expect(result).toBe("Hello World!");
+  });
+
+  it("should colorize the warn level", () => {
+    const result = colorize().message("Hello World!", LogLevel.WARN);
+    expect(result).toBe(chalk.yellow("Hello World!"));
+  });
+
+  it("should colorize the error level", () => {
+    const result = colorize().message("Hello World!", LogLevel.ERROR);
+    expect(result).toBe(chalk.red("Hello World!"));
   });
 });
