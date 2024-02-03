72d25765f089ab8557ad48acc9e05b5644903250
MDReal32 <veysaliyev00@gmail.com>
2024-02-03 09:06:32 +0400

    chore(scripts): improve testing samples

--- scripts/test-sample.ts
+++ scripts/test-sample.ts
@@ -1,4 +1,4 @@
-import { existsSync } from "node:fs";
+import { existsSync, readdirSync } from "node:fs";
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 
@@ -7,15 +7,27 @@ if (process.argv.length < 3) {
   process.exit(1);
 }
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const name = process.argv[2];
-const sample = resolve(__dirname, "../samples", `${name}.ts`);
-if (!existsSync(sample)) {
-  console.error(`Sample ${name} not found`);
-  process.exit(1);
-}
-
-(async () => {
+const testModule = async (sample: string) => {
   const { test } = await import(sample);
   await test();
-})();
+};
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const name = process.argv[2];
+if (["-a", "--all"].includes(name)) {
+  const samples = readdirSync(resolve(__dirname, "../samples"));
+  for (const sample of samples) {
+    if (sample.endsWith(".ts")) {
+      console.log(`Testing ${sample}`);
+      await testModule(resolve(__dirname, "../samples", sample));
+      console.log(`Sample ${sample} passed!\n\n`);
+    }
+  }
+} else {
+  const sample = resolve(__dirname, "../samples", `${name}.ts`);
+  if (!existsSync(sample)) {
+    console.error(`Sample ${name} not found`);
+    process.exit(1);
+  }
+  await testModule(sample);
+}
